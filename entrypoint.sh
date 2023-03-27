4e8310f928cd6129597e9726c7f7d200ba5d0048
ayarosh <8566354+ayarosh@users.noreply.github.com>
2023-03-27 12:46:02 +0300

    updated the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 # add additional dependencies required for your solution here.
 # for example:
 # pip3 install mysql-client
+# sleep infinity
 
-sleep infinity
+pip3 install -r /scripts/requirements.txt
+python3 /scripts/update.py
